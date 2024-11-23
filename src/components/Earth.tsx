9172b69ef64319fd0948d0a6752c0c0b351b9d93
Gstx <geekhous3@outlook.com>
2024-11-23 19:51:23 -0300

    Update Earth.tsx

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { PerspectiveCamera, useGLTF, OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
